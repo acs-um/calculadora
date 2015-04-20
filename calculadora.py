a198857993cbbbbbd5e036e095891cf96f2425b5
Matías Varela <matu.varela@gmail.com>
2015-04-20 19:06:10 -0300

    Añado funcion de potenciación

--- calculadora.py
+++ calculadora.py
@@ -2,4 +2,8 @@
 #!/usb/bin/env python2
 
 class Calculadora:
-    pass
+    def potencia(self, num1, num2):
+        """
+        Realiza la potencia de num1 en base a num2.
+        """
+        return num1**num2
